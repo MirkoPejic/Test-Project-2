a0d8c475bb1a1cae07c8127121982f837d8fe3a2
MirkoPejic <mirko.pejic1@gmail.com>
2016-02-19 12:10:58 +0100

    version 2.0

---
+++ Project.Service/Maker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Project.DAL;
+
+namespace Project.Service
+{
+    public class Maker
+    {
+        public IEnumerable<VehicleMake> VehicleMaker
+        {
+            get
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["VehiclesDBEntities"].ConnectionString;
+                List<VehicleMake> vehicleManufacturer = new List<VehicleMake>();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllVehicleMaker", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        VehicleMake vehicleMake = new VehicleMake();
+                        vehicleMake.Id = Convert.ToInt32(rdr["Id"]);
+                        vehicleMake.VehicleName = rdr["VehicleName"].ToString();
+                        vehicleMake.VehicleAbbreviation = rdr["VehicleAbbreviation"].ToString();
+
+                        vehicleManufacturer.Add(vehicleMake);
+                    }
+                }
+                return vehicleManufacturer;
+            }
+        }
+    }
+}
---
+++ Project2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Service;
+using Project.DAL;
+
+namespace Project2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Maker maker = new Maker();
+            List<VehicleMake> vehicleMaker = maker.VehicleMaker.ToList();
+            return View(vehicleMaker);
+        }
+    }
+}
